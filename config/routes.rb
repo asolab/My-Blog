7c8d45718036307825e5725e931455a6a0c2c971
asolab <asolabofficial@gmail.com>
2022-11-08 16:40:56 +0100

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+
+  resources :posts do 
+
+    resources :comments
+
+    
+  end
+    
+  
+  root "posts#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
